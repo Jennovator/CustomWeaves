0662c64e2cf66e298e4fbeb8fd35f46be9277e13
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-10-25 14:01:34 -0400

    #Update: removed duplicate data for image extraction from response

--- server/routes/ai.routes.js
+++ server/routes/ai.routes.js
@@ -7,32 +7,30 @@ dotenv.config(); //set-up environment variables
 const router = express.Router(); // new additional routes
 
 // configure the use of the API (Key from your account API keys)
-const config = new OpenAI({
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-})
-
-// utilize the config and merge it with the openai api
-const openai = new OpenAI(config);
+});
 
 // Route
 router.route('/').get((req, res) => {
     res.status(200).json({ message: "Hello from AI ROUTES"})
-})
+});
 
 // a route that will pass the prompt from the frontend to the server
 router.route('/').post( async(req, res) => {
     try{
         const {prompt} = req.body;
 
-        const response = await openai.createImage({
+        const response = await openai.images.generate({
             prompt, // prompt from the frontend
             n: 1, // number of images to be processed
             size: '1024x1024', // size of the image
             response_format: 'b64_json' // base 64 json 
         });
 
+        
         // get the image out of the response
-        const image = response.data.data[0].b64_json;
+        const image = response.data[0].b64_json;
 
         // pass the result image to the frontend
         res.status(200).json({ photo: image });
@@ -42,4 +40,5 @@ router.route('/').post( async(req, res) => {
         res.status(500).json({ message: "Something went wrong"})
     }
 })
+
 export default router;
