6066bde0eeab9363258811a2a2142bb73af1796a
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-10-25 14:02:33 -0400

    #Update: change routes to dalle

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ dotenv.config(); //set-up environment variables
 const app = express(); // set-up express ap
 // set-up middlewares
 app.use(cors());
-app.use(express.json({ limit: "50mb"})) // specify the weight of the payload that it can send
+app.use(express.json({ limig: "50mb"})) // specify the weight of the payload that it can send
 // consume the ai routes from routes
-app.use("/api/v1/ai", aiRoutes);
+app.use("/api/v1/dalle", aiRoutes);
 
 app.get('/', (req,res) => {
     res.status(200).json({ message: "Hello from AI"})
