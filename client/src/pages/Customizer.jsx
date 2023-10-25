284748fd4fbad0b1fddc3bd6965bce4ef8b2870c
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-10-25 15:58:15 -0400

    #Update: for final deployment

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -62,7 +62,7 @@ const Customizer = () => {
       // call the backend to generate an AI image
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://fabrikthreejs.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
